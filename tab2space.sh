4709a1f7b3fb9142bceea1c2057640897e6e87c4
S.Çağlar Onur <caglar@pardus.org.tr>
2006-10-12 21:34:48 +0000

    artık sanada gerek yok

--- tab2space.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ..
-find -name pspec.xml | xargs sed -i -e "s~\t~    ~g"
-find -name actions.py | xargs sed -i -e "s~\t~    ~g"
-find -name pspec.xml | xargs sed -i -e "s~^    <Desc~        <Desc~g"
-find -name pspec.xml | xargs sed -i -e "s~^    <Sum~        <Sum~g"
-find -name pspec.xml | xargs sed -i -e "s~^    <IsA~        <IsA~g"

41120f77bda15cf876ee3232d3127620ed927e4f
Görkem Çetin <gorkem@pardus.org.tr>
2006-12-02 11:49:25 +0000

    cleaner

--- pspec-translations/import-all-languages.sh
+++ pspec-translations/import-all-languages.sh
@@ -2,8 +2,8 @@
 
 # Extract all the po files from the pspecs.
 
-./pspec2po.py import ../../../../pardus/devel tr tr.po
-./pspec2po.py import ../../../../pardus/devel de de.po
-./pspec2po.py import ../../../../pardus/devel nl nl.po
-./pspec2po.py import ../../../../pardus/devel es es.po
-
+for i in tr de nl es 
+do
+./pspec2po.py import ../../../../pardus/devel $i $i.po
+done
+=

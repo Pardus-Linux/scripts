c2f9d5fa184d8dc483f02786611d419f5fb8ace3
Görkem Çetin <gorkem@pardus.org.tr>
2007-03-24 22:02:49 +0000

    update for catalan

--- pspec-translations/extract-all-languages.sh
+++ pspec-translations/extract-all-languages.sh
@@ -2,7 +2,7 @@
 
 # Extract all the po files from the pspecs.
 
-for i in tr de nl es pt_BR fr it
+for i in tr de nl es pt_BR fr it ca
 do 
 ./pspec2po.py extract ../../../../pardus/devel $i $i.po
 done

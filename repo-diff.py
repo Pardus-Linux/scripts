53045cb72562ca3daa997c8c2523dc8aa090008e
S.Çağlar Onur <caglar@pardus.org.tr>
2007-06-08 05:45:51 +0000

    oscar goes to Hüseyin Akcan

--- repo-diff.py
+++ repo-diff.py
@@ -32,18 +32,40 @@ def getVersion(pspecList):
         sources[specFile.source.name] = (specFile.getSourceVersion(), specFile.getSourceRelease())
     return sources
 
-def listIntersection(firstRepo, secondRepo):
-    keys = list(set(firstRepo.keys()).__and__(set(secondRepo.keys())))
-    keys.sort()
-    for i in keys:
-        if firstRepo[i] != secondRepo[i]:
-            print "    %s: %s (r%s) -> %s (r%s)" % (i, firstRepo[i][0], firstRepo[i][1], secondRepo[i][0], secondRepo[i][1])
-
-def listComplement(firstRepo, secondRepo):
-    keys = list(set(firstRepo.keys()) - set(secondRepo.keys()))
-    keys.sort()
-    for i in keys:
-        print "    %s" % i
+def listCompare(firstRepo, secondRepo):
+    # form two sorted lists and 
+    # find differences in a single pass
+    # over the lists
+    keys1 = list(firstRepo.keys())
+    keys2 = list(secondRepo.keys())
+    keys1.sort()
+    keys2.sort()
+    onlyinFR = list()
+    onlyinSR = list()
+    diffVersion = list()
+    diffRelease = list()
+    i=j=0
+    len1 = len(keys1)
+    len2 = len(keys2)
+    while i < len1:
+       while j < len2:
+          if keys1[i] < keys2[j]:
+             onlyinFR.append(keys1[i])
+             i=i+1
+          elif keys1[i] > keys2[j]:
+             onlyinSR.append(keys2[j])
+             j=j+1
+          else:
+            if firstRepo[keys1[i]][0] != secondRepo[keys2[j]][0]:
+              diffVersion.append(keys1[i])
+            elif firstRepo[keys1[i]][1] != secondRepo[keys2[j]][1]:
+              diffRelease.append(keys1[i])
+            #else:
+              #same version and release, nothing to do
+            i = i+1
+            j = j+1
+    
+    return (onlyinFR, onlyinSR, diffVersion, diffRelease)
 
 def usage(miniMe):
     print """Usage:
@@ -65,15 +87,26 @@ if __name__ == "__main__":
 
     stable = getVersion(findPspec(join_path(svnRoot,"2007/", postfix)))
     devel = getVersion(findPspec(join_path(svnRoot, "devel/", postfix)))
+    
+    # find differences in given repos
+    r = listCompare(stable, devel)
+    
+    print "Stable --> Devel (different version)"
+    for i in r[2]:
+       print "    %s: %s (r%s) -> %s (r%s)" % (i, stable[i][0], stable[i][1], devel[i][0], devel[i][1])
+    print
 
-    print "Stable --> Devel"
-    listIntersection(stable, devel)
+    print "Stable --> Devel (different release)"
+    for i in r[3]:
+       print "    %s: %s (r%s) -> %s (r%s)" % (i, stable[i][0], stable[i][1], devel[i][0], devel[i][1])
     print
 
     print "Stable has, Devel hasn't"
-    listComplement(stable, devel)
+    for i in r[0]:
+       print "    %s" % i
     print
 
     print "Devel has, Stable hasn't"
-    listComplement(devel, stable)
+    for i in r[1]:
+       print "    %s" % i
     print

876f210eef0a3520cc01301dce4d1b184031a087
S.Çağlar Onur <caglar@pardus.org.tr>
2007-06-14 20:04:58 +0000

    translations.xml ile bunların ömrü bitti

--- turkceleri-eksik-pspecleri-bul.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# cd to pardus devel ... change it necessarily
-for p in `find ../../../pardus/devel/ -name pspec.xml`; do grep 'xml:lang' $p > /dev/null; \
-if [ $? == "1" ]; then echo $p; fi; done > turkceleri-eksik-pspecler

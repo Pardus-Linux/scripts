9aac7481f538d8f78a7b5741e21a6df3d8b74e0b
Semen Cirit <scirit@pardus.org.tr>
2009-01-29 12:18:00 +0000

    a script for making a texlive-core tarball.

---
+++ texlive/pack.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+VERSION="2008"
+
+for option in $*; do
+    case $option in
+        --mirror=*)
+            MIRROR="`echo $option | awk -F= '{print $2;}'`"
+        ;;
+        --version=*)
+            VERSION="`echo $option | awk -F= '{print $2;}'`"
+        ;;
+        --package=*)
+            PACKAGE="`echo $option | awk -F= '{print $2;}'`"
+        ;;
+        --filelist=*)
+            FILELIST="`echo $option | awk -F= '{print $2;}'`"
+        ;;
+    esac
+done
+
+if [ "$MIRROR" == "" ]; then
+    echo "Use with $0 --mirror=http://ftp://ftp.linux.org.tr/pub/gentoo/distfiles/"
+    exit
+fi
+
+if [ "$PACKAGE" == "" ]; then
+    echo "Use with $0 --package=texlive-core-modules"
+    exit
+fi
+
+if [ "$FILELIST" == "" ]; then
+    echo "Use with $0 --filelist=modulelist"
+    exit
+fi
+
+ARCHIVES_DIR=~/texlive-archives
+
+if [ ! -e $ARCHIVES_DIR ]; then
+    mkdir $ARCHIVES_DIR
+fi
+
+SCRIPT_DIR=`pwd`
+TEMP_DIR=$SCRIPT_DIR/$PACKAGE-$VERSION
+
+mkdir $TEMP_DIR
+cd $ARCHIVES_DIR
+
+for i in `cat $SCRIPT_DIR/$FILELIST`
+do
+    if [ ! -e $i ]; then
+        wget $MIRROR"/"$i || exit 1
+        echo "$i" > $FILELIST.changes
+    fi
+    tar --lzma -xv -f $i -C $TEMP_DIR
+done
+
+cd $SCRIPT_DIR
+tar cjvf $PACKAGE-$VERSION.tar.bz2 -C $TEMP_DIR .
+rm -rf  $TEMP_DIR
+
+sha1sum $PACKAGE-$VERSION.tar.bz2

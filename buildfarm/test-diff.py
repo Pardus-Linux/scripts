8b906d5bac6fa28bd37cf6ed5bf32f6680efb728
Ekin Meroğlu <ekin@pardus.org.tr>
2009-02-16 07:57:23 +0000

    no more 'sort -u' :-P

--- buildfarm/test-diff.py
+++ buildfarm/test-diff.py
@@ -3,6 +3,12 @@
 
 import os
 
+packages = []
 for i in os.listdir("/var/cache/pisi/packages-test/"):
     if not os.path.exists("/var/cache/pisi/packages-stable/%s" % i):
+        packages.append(i)
+
+if (packages):
+    packages.sort()
+    for i in packages:
         print i

8ef1bc8ae108e2f617c106e4d019b25e0874e830
Ozan Çağlayan <ozan@pardus.org.tr>
2009-07-24 06:46:43 +0000

    Optimizationssssssssssss

--- checkicons.py
+++ checkicons.py
@@ -12,4 +12,7 @@ for p in [_p for _p in pisi.api.list_installed() if not _p.startswith("cursor-th
     iconlist = ["/%s" % f.path for f in installdb.get_files(p).list if f.path.startswith(("usr/share/icons", "usr/share/pixmaps"))]
     if iconlist and not packagedb.get_package(p).icon:
         appgui = "app:gui" in packagedb.get_package(p).isA
+        if len(iconlist) > 5:
+            iconlist = iconlist[:5]
+            iconlist.append("...")
         print "\n%s (app:gui=%s)\n\n\t%s" % (p, appgui, "\n\t".join(iconlist))

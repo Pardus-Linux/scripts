3dd3167221228d2fe6955d8f259b1ae4c1a1fb2a
Semen Cirit <scirit@pardus.org.tr>
2010-01-14 12:44:49 +0000

    Bug activity table is also updated during svn commit.

--- svn/svn-bugs.py
+++ svn/svn-bugs.py
@@ -6,9 +6,15 @@ import os
 import sys
 
 COMMENT_SQL = """INSERT INTO longdescs (bug_id, who, bug_when, thetext, work_time, isprivate) VALUES (%(bug_id)s, %(user_id)d, '%(bug_when)s', '%(thetext)s', 0.0, 0)"""
+
+COMMENT_BUG_ACTIVITY_STATUS = """INSERT INTO bugs_activity (bug_id, who, bug_when, fieldid, added, removed) VALUES ('%(bug_id)s', %(user_id)d, '%(bug_when)s', %(fieldid)d, '%(added)s', '%(removed)s')"""
+COMMENT_BUG_ACTIVITY_RESOLUTION = """INSERT INTO bugs_activity (bug_id, who, bug_when, fieldid, added, removed) VALUES ('%(bug_id)s', %(user_id)d, '%(bug_when)s', %(fieldid)d, '%(added)s', '%(removed)s')"""
 #BUGS_COMMENT_SQL = "UPDATE bugs SET lastdiffed='%(cur_time)s' WHERE bug_id=%(bug_id)s"
 BUGS_SQL = "UPDATE bugs SET bug_status='RESOLVED',resolution='FIXED' WHERE bug_id=%(bug_id)s"
+
 USERID_SQL = "SELECT userid FROM `profiles` WHERE login_name='%(mail)s'"
+STATUS_SQL = "SELECT bug_status FROM `bugs` WHERE bug_id='%(bug_id)s'"
+RESOLUTION_SQL = "SELECT resolution FROM `bugs` WHERE bug_id='%(bug_id)s'"
 
 COMMENT_TEMPLATE = u"""Author: %(author)s
 Repository: %(repo)s
@@ -24,9 +30,9 @@ Commit Message:
 def getAuthorMail(author):
     f = open("/uludag/trunk/common/accounts")
     for a in f:
-    	a = a.split(":")
-	if a[0] == author:
-	    return a[2].replace(" [at] ", "@")
+        a = a.split(":")
+    if a[0] == author:
+        return a[2].replace(" [at] ", "@")
 
 def checkBUG(line):
     line = line.strip().split(':')
@@ -49,7 +55,7 @@ def main(author, log, commit_no, changed, repo):
     thetext=thetext.replace("(", "\\(")
     thetext=thetext.replace(";", "")
     open("/tmp/svn-bugs.log", "w").write(thetext)
-    
+
     db = mysql.connect(host="localhost", user="", passwd="", db="")
     cur = db.cursor()
 
@@ -57,13 +63,35 @@ def main(author, log, commit_no, changed, repo):
 
 
     def getAuthorBugzillaID():
-    	author_mail = getAuthorMail(author)
-    	if cur.execute(USERID_SQL % {"mail": author_mail}) == 1:
-	    return cur.fetchone()[0]
-	return 1
+        author_mail = getAuthorMail(author)
+        if cur.execute(USERID_SQL % {"mail": author_mail}) == 1:
+            return cur.fetchone()[0]
+        return 1
+
+    def getOldBugStatus():
+        if cur.execute(STATUS_SQL % {"bug_id": bug_id}) == 1:
+            return cur.fetchone()[0]
+        return 1
+
+    def getOldResolution():
+         if cur.execute(RESOLUTION_SQL % {"bug_id": bug_id}) == 1:
+             return cur.fetchone()[0]
+         return 1
+
 
     def commentBUG(bug_id):
         # FIXME author is 1...
+
+        fieldid = 8
+        added = "RESOLVED"
+        cur.execute(COMMENT_BUG_ACTIVITY_STATUS % {"bug_id": bug_id, "user_id": getAuthorBugzillaID(), "bug_when": cur_time, "fieldid" : fieldid, "added" : added, "removed": getOldBugStatus()})
+        db.commit()
+
+        fieldid = 11
+        added = "FIXED"
+        cur.execute(COMMENT_BUG_ACTIVITY_RESOLUTION % {"bug_id": bug_id, "user_id": getAuthorBugzillaID(), "bug_when": cur_time, "fieldid" : fieldid, "added" : added, "removed": getOldBugResolution()})
+        db.commit()
+
         cur.execute(COMMENT_SQL % {"bug_id": bug_id, "user_id": getAuthorBugzillaID(), "bug_when": cur_time, "thetext": thetext})
         db.commit()
 #        cur.execute(BUGS_COMMENT_SQL % {"bug_id": bug_id, "cur_time": cur_time})
@@ -85,19 +113,19 @@ if __name__ == "__main__":
     SVNLOOK='/usr/bin/svnlook'
 
     try:
-    	repo = sys.argv[1]
-    	commit_no = sys.argv[2]
+        repo = sys.argv[1]
+        commit_no = sys.argv[2]
         cmd = '%s log -r %s %s' % (SVNLOOK, commit_no, repo)
         log = os.popen(cmd, 'r').readlines()
 
         cmd = '%s author -r %s %s' % (SVNLOOK, commit_no, repo)
         author = os.popen(cmd, 'r').readline().rstrip('\n')
 
-	cmd = '%s changed -r %s %s' % (SVNLOOK, commit_no, repo)
-	changed = os.popen(cmd, 'r').readlines()
+    cmd = '%s changed -r %s %s' % (SVNLOOK, commit_no, repo)
+    changed = os.popen(cmd, 'r').readlines()
 
         main(author, log, commit_no, changed, repo)
 
     except Exception, e:
-    	open("/tmp/svn-bugs.err", "w").write("error\n%s" % e)
+        open("/tmp/svn-bugs.err", "w").write("error\n%s" % e)
 
